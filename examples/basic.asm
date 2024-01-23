fca827032020aed33dda10bbf19301429db03bd1
puffer <kevin@alavik.se>
2024-01-23 11:47:51 +0000

    Added some test things

--- examples/basic.asm
+++ examples/basic.asm
@@ -1,10 +1,1 @@
-; Simple example using SCAP-assembly
-
-; MV instruction - Move immediate value 1 to register A
-MV A, 0x0001
-
-; ADD instruction - Add register A and immediate value 1, store result in register B
-ADD B, A, 0x0001
-
-; Halt the program (optional)
-NOP
+jmp 0xFF
