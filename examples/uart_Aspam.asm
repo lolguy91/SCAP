4c7985ff0bc3d8da2e4ef741b1cc1e26b58e198e
lolguy91 <retek1544@gmail.com>
2024-01-23 16:44:29 +0000

    clarification

--- examples/uart_Aspam.asm
+++ examples/uart_Aspam.asm
@@ -1,4 +1,4 @@
-ld A 0x0009
-st A 0xFF00
-jmp 0x0000
-db 0x41
+ld A 0x0009 ;load the the letter A stored at 0x0009 into A
+st A 0xFF00 ;print the letter to UART (0xFF00)
+jmp 0x0000 ;jump back to the beginning
+db 0x41 ;store the letter A
