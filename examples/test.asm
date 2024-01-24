03d9e25e4ebc76caabc4588518ebfade77dc8de6
puffer <kevin@alavik.se>
2024-01-24 08:07:21 +0000

    idk at this point

--- examples/test.asm
+++ examples/test.asm
@@ -1,9 +1,9 @@
 jmp $main
-;data section
-the_a: db 0x41 
+ 
 
-;main function
+disable_keyboard:
+    ld A 0x00
+    st A 0xFF00  
+
 main:
-    ld A $the_a    ;load the the letter A into A
-    st A 0xFF00    ;print the letter to UART (0xFF00)
-    jmp 0x0000     ;jump back to the beginning
+    jmp $disable_keyboard
