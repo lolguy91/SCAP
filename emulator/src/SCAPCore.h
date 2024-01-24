bc22d8362cf8f0144afacbf9e65cb6bc8d6481fa
puffer <kevin@alavik.se>
2024-01-24 11:33:34 +0000

    Added a register dump signal (debug)

--- emulator/src/SCAPCore.h
+++ emulator/src/SCAPCore.h
@@ -3,6 +3,12 @@
 
 #include <stdbool.h>
 
+extern uint8_t A;
+extern uint8_t B;
+extern uint16_t PC;
+extern uint16_t SP;
+extern uint8_t flags;
+
 bool step_scap();
 void scap_init(bool debug_mode);
 void scap_interrupt();
